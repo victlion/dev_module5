8c9e025277065f89b470976e984d6604d5c42cdf
victlion <victlionpro@gmail.com>
2023-12-11 15:34:20 +0200

    fix Calculation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,16 +6,10 @@ public class Main {
     public static void main(String[] args) {
         GetIntFibonacci getIntFibonacci = new GetIntFibonacci();
 
-        // O(n)
-        // O(1) Memory
         System.out.println("Iterator : " + getIntFibonacci.Iterator(6));
 
-        // O(2^n)
-        // O(n) Memory
         System.out.println("Recursion : " + getIntFibonacci.Recursion(6));
 
-        // O(n)
-        // O(n) Memory
-        System.out.println("Dynamic : " + getIntFibonacci.Dynamic(6));
+        System.out.println("Dynamic : " + getIntFibonacci.Dynamic(4));
     }
 }
--- src/main/java/org/example/fibonacci/GetIntFibonacci.java
+++ src/main/java/org/example/fibonacci/GetIntFibonacci.java
@@ -2,20 +2,27 @@ package org.example.fibonacci;
 
 import java.util.Arrays;
 
-//0, 1, 1, 2, 3, 5, 8, 13, 21
 public class GetIntFibonacci {
-    public int Iterator(int n) {
-        int number = 0, second = 1;
 
-        for (int i = 0; i < n; i++) {
-            int next = number + second;
+    // 3 + 4 * 6 + 1 = 28 => 4 + 4n => 4n
+    // Memory = o(4)
+    public int Iterator(int n) {
+        int number = 0; //1
+        int second = 1; //1
+        int next = 0;   //1
+
+        for (int i = 0; i < n; i++) {//4
+            next = number + second;
             number = second;
             second = next;
         }
-        return number;
+        return number;//1
     }
 
-    public int Recursion(int n){
+
+    //=> n^2
+    //Memory 2^0 + 2^1 + 2^2 + 2^3 + 2^4 + 2^5 + 2^6 o(127)
+    public int Recursion(int n) {
         if (n <= 1) {
             return n;
         } else {
@@ -23,6 +30,9 @@ public class GetIntFibonacci {
         }
     }
 
+
+    //6 + 2n => 2n
+    //Memory o(2)
     public int Dynamic(int n) {
         int[] fibonacciArray = new int[n];
         fibonacciArray[0] = 0;
